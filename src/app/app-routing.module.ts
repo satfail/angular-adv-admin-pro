2f5c89bfd5e15035bc195ed3f2271db6af1b932e
= <=>
2021-02-16 15:53:35 +0100

    Fin seccion 4 Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,33 +1,16 @@
-import { Route } from '@angular/compiler/src/core';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
-import { ProgressComponent } from './pages/progress/progress.component';
+import { AuthRoutingModule } from './auth/auth.routing';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { PagesRoutingModule } from './pages/pages.routing';
+
 
 
 const routes: Routes = [
 
-  //Como la página pages va a contener a los usuarios loguedos
-  //meto como hijas a las que se van a visualizar
-  {
-    path:'',
-    component:PagesComponent,
-    children:[
-      {path:'dashboard', component:DashboardComponent},
-      {path:'grafica1', component: Grafica1Component},
-      {path:'progress', component: ProgressComponent},
-      {path:'', redirectTo : '/dashboard', pathMatch:'full'},
-    ],
-
-  },
-  {path:'login', component: LoginComponent},
-  {path:'register', component: RegisterComponent},
-
+  // path : /dashboard  PagesRouting
+  // auth : /auth       AuthRouting
+  {path:'', redirectTo : '/dashboard', pathMatch:'full'},
   {path:'**', component: NopagefoundComponent},
 ];
 
@@ -35,7 +18,9 @@ const routes: Routes = [
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot( routes )
+    RouterModule.forRoot( routes ),
+    PagesRoutingModule,
+    AuthRoutingModule,
   ],
   exports:[RouterModule]
 })
